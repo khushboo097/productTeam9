f0decaf00af09aacf8c98017e1e4e293cd680e23
Mohit Shah <44272065+shahmohit9975@users.noreply.github.com>
2020-01-19 12:05:50 +0530

    first commit

---
+++ src/main/java/com/coviam/team9/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package com.coviam.team9.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
